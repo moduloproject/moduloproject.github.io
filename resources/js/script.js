6624e3ade74b1f78b988acd6c753f50ec247c434
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-01-07 19:04:41 +0100

    Automagic redirection to https protocol

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,9 @@
+// Redirection to https protocol
+var host = "moduloteam.github.io";
+if ((host == window.location.host) && (window.location.protocol != "https:"))
+    window.location.protocol = "https";
+
+
 // Twitter API
 ! function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
