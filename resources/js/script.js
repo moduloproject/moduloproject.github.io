a277981f76ab23be47d562347278815e8226de79
Philippe Loctaux <loctauxphilippe@users.noreply.github.com>
2015-05-09 23:31:59 +0200

    duplicate of secure.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,3 @@
-// Redirection to https protocol
-var host = "moduloteam.github.io";
-if ((host == window.location.host) && (window.location.protocol != "https:"))
-    window.location.protocol = "https";
-
-
 // Twitter API
 ! function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
